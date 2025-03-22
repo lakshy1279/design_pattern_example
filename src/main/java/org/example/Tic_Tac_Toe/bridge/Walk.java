3ef536132d0f956987d996cc290a50cdc43afff3
lakshy1279 <luckyron1279@gmail.com>
2025-03-22 12:29:23 +0530

    add another example for builder and strategy

--- src/main/java/org/example/Tic_Tac_Toe/bridge/Walk.java
+++ src/main/java/org/example/Tic_Tac_Toe/bridge/Walk.java
@@ -6,3 +6,78 @@ public class Walk implements MoveLogic{
         System.out.println("Waling Using legs");
     }
 }
+
+//another example
+// Implementor
+interface PlatformAPI {
+    void renderVideo(String fileName);
+}
+
+// Concrete Implementors
+class WindowsAPI implements PlatformAPI {
+    public void renderVideo(String fileName) {
+        System.out.println("Rendering on Windows: " + fileName);
+    }
+}
+class MacAPI implements PlatformAPI {
+    public void renderVideo(String fileName) {
+        System.out.println("Rendering on Mac: " + fileName);
+    }
+}
+
+// Abstraction
+abstract class VideoPlayer {
+    protected PlatformAPI platform;
+    
+    public VideoPlayer(PlatformAPI platform) {
+        this.platform = platform;
+    }
+    
+    abstract void play(String fileName);
+}
+
+
+// Refined Abstraction
+class AdvancedVideoPlayer extends VideoPlayer {
+    private PlaybackStrategy strategy;
+
+    public AdvancedVideoPlayer(PlatformAPI platform, PlaybackStrategy strategy) {
+        super(platform);
+        this.strategy = strategy;
+    }
+
+    @Override
+    void play(String fileName) {
+        platform.renderVideo(fileName);    // Bridge part
+        strategy.execute(fileName);        // Strategy part
+    }
+}
+
+
+// Refined Abstraction
+class AdvancedVideoPlayer extends VideoPlayer {
+    private PlaybackStrategy strategy;
+
+    public AdvancedVideoPlayer(PlatformAPI platform, PlaybackStrategy strategy) {
+        super(platform);
+        this.strategy = strategy;
+    }
+
+    @Override
+    void play(String fileName) {
+        platform.renderVideo(fileName);    // Bridge part
+        strategy.execute(fileName);        // Strategy part
+    }
+}
+
+
+public class Client {
+    public static void main(String[] args) {
+        PlatformAPI platform = new WindowsAPI();
+        PlaybackStrategy strategy = new FastForwardPlayback();
+
+        VideoPlayer player = new AdvancedVideoPlayer(platform, strategy);
+        player.play("my_movie.mp4");
+    }
+}
+
